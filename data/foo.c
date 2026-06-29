0f7a1daedce9827fa674f68e69bd90db02589063
estevesnp <nuno.pinto@live.com>
2026-06-29 03:44:14 +0100

    feat: initial schema implementation

--- data/foo.c
+++ data/foo.c
@@ -1,3 +1,12 @@
+struct Foo {
+    const char *name;
+    int age;
+};
+
+typedef struct Bar {
+    int bariarity;
+} Bar;
+
 int calculate(int a, int b) {
     int c = a + b;
     int d = b * c;
--- src/main.zig
+++ src/main.zig
@@ -1,17 +1,149 @@
 const std = @import("std");
 const Io = std.Io;
 const assert = std.debug.assert;
+const fatal = std.process.fatal;
 
 const aro = @import("aro");
 
 const fff_h = "data/fff.h";
 
-fn handleTree(io: Io, tree: *aro.Tree) !void {
-    var stdout_buf: [1024]u8 = undefined;
-    var stdout = Io.File.stdout().writer(io, &stdout_buf);
-    const term: Io.Terminal = .{ .mode = .escape_codes, .writer = &stdout.interface };
+const Kind = enum {
+    scalar,
+    @"struct",
+    @"enum",
+    @"union",
+    pointer,
+    // TODO: function? for function pointers (callbacks, etc)
+};
 
-    try tree.dump(term);
+const Type = union(Kind) {
+    scalar: []const u8,
+    @"struct": []const u8,
+    @"enum": []const u8,
+    @"union": []const u8,
+
+    pointer: *Property,
+};
+
+const Schema = struct {
+    structs: []const Struct,
+    enums: []const Enum,
+    unions: []const Union,
+    functions: []const Function,
+};
+
+const Struct = struct {
+    name: []const u8,
+    properties: []const Property,
+};
+
+const Enum = struct {
+    name: []const u8,
+    backing_type: []const u8,
+    values: []const Value,
+
+    const Value = struct {
+        name: []const u8,
+        value: u32,
+    };
+};
+
+const Union = struct {
+    name: []const u8,
+    variants: []const Property,
+};
+
+const Function = struct {
+    name: []const u8,
+    parameters: []const Property,
+    return_type: Property,
+};
+
+const Property = struct {
+    name: ?[]const u8 = null,
+    @"const": bool = false,
+    kind: Kind,
+    type: Type,
+};
+
+fn parseTree(arena: std.mem.Allocator, tree: *aro.Tree) !Schema {
+    const comp = tree.comp;
+
+    var functions: std.ArrayList(Function) = .empty;
+    defer functions.deinit(arena);
+
+    var structs: std.ArrayList(Struct) = .empty;
+    defer structs.deinit(arena);
+
+    var enums: std.ArrayList(Enum) = .empty;
+    defer enums.deinit(arena);
+
+    var unions: std.ArrayList(Union) = .empty;
+    defer unions.deinit(arena);
+
+    for (tree.root_decls.items) |node_idx| {
+        const node = node_idx.get(tree);
+        const node_qt = node_idx.qtOrNull(tree) orelse continue;
+
+        switch (node) {
+            .function => |function| {
+                // TODO - properly parse function
+                const func: Function = .{
+                    .name = tree.tokSlice(function.name_tok),
+                    .parameters = &.{},
+                    .return_type = .{ .name = "void", .kind = .scalar, .type = .{ .scalar = "void" } },
+                };
+                try functions.append(arena, func);
+            },
+            .struct_decl => |struct_decl| {
+                _ = struct_decl;
+                const struct_type = switch (node_qt.type(comp)) {
+                    .@"struct" => |s| s,
+                    else => |e| {
+                        std.debug.print("unexpected type for struct_decl: {t}\n", .{e});
+                        continue;
+                    },
+                };
+
+                var properties: std.ArrayList(Property) = .empty;
+                defer properties.deinit(arena);
+
+                for (struct_type.fields) |field| {
+                    // TODO - properly parse fields
+                    const prop: Property = .{
+                        .name = field.name.lookup(comp),
+                        .kind = .scalar,
+                        .type = .{ .scalar = "void" },
+                    };
+                    try properties.append(arena, prop);
+                }
+
+                const st: Struct = .{
+                    .name = struct_type.name.lookup(comp),
+                    .properties = try properties.toOwnedSlice(arena),
+                };
+
+                try structs.append(arena, st);
+            },
+            .enum_decl => |enum_decl| {
+                std.debug.print("TODO: enum_decl={any}\n", .{enum_decl});
+            },
+            .union_decl => |union_decl| {
+                std.debug.print("TODO: union_decl={any}\n", .{union_decl});
+            },
+            else => {
+                std.debug.print("skipping node {t}\n", .{node});
+                continue;
+            },
+        }
+    }
+
+    return .{
+        .functions = try functions.toOwnedSlice(arena),
+        .structs = try structs.toOwnedSlice(arena),
+        .enums = try enums.toOwnedSlice(arena),
+        .unions = try unions.toOwnedSlice(arena),
+    };
 }
 
 pub fn main(init: std.process.Init) !void {
@@ -86,5 +218,15 @@ pub fn main(init: std.process.Init) !void {
     var tree = try pp.parse();
     defer tree.deinit();
 
-    try handleTree(io, &tree);
+    const decls = try parseTree(arena, &tree);
+    var stdout_buf: [1024]u8 = undefined;
+    var stdout = Io.File.stdout().writer(io, &stdout_buf);
+
+    try stdout.interface.print("{f}\n", .{
+        std.json.fmt(decls, .{
+            .whitespace = .indent_2,
+            .emit_null_optional_fields = false,
+        }),
+    });
+    try stdout.interface.flush();
 }
